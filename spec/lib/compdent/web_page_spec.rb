b7c00b3d45e90095a5493c5d213da8d260ba65f9
robmckinnon <rob@theyworkforyou.co.nz>
2012-07-08 22:07:42 +0100

    refactoring

--- spec/lib/compdent/web_page_spec.rb
+++ spec/lib/compdent/web_page_spec.rb
@@ -7,22 +7,22 @@ describe WebPage do
   describe "asked for canonical URI" do
     context "for host uri with trailing slash" do
       it 'should return uri unmodified' do
-        WebPage.canonical_uri('http://example.com/').should == 'http://example.com/'
-
+        WebPage.canonical_uri('http://example.com/?a=b&x=y').
+          should == 'http://example.com/?a=b&x=y'
       end
     end
 
     context "for host uri without trailing slash" do
       it 'should return uri with trailing slash' do
-        WebPage.canonical_uri('http://example.com?a=b&x=y').should == 'http://example.com/?a=b&x=y'
-
+        WebPage.canonical_uri('http://example.com?a=b&x=y').
+          should == 'http://example.com/?a=b&x=y'
       end
     end
 
     context "for uri with path after host" do
       it 'should return uri unmodified' do
-        WebPage.canonical_uri('http://example.com/more?a=b&x=y').should == 'http://example.com/more?a=b&x=y'
-
+        WebPage.canonical_uri('http://example.com/more?a=b&x=y').
+          should == 'http://example.com/more?a=b&x=y'
       end
     end
   end
