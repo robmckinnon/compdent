944660ab794fa80e1cd769a816ea34cb57a79b74
robmckinnon <rob@theyworkforyou.co.nz>
2012-07-08 18:10:53 +0100

    created config directory

--- lib/compdent.rb
+++ lib/compdent.rb
@@ -13,4 +13,4 @@ require File.dirname(__FILE__) + '/compdent/twitter_scraper'
 require 'grackle'
 
 ENV['MONGOID_ENV'] ||= 'development'
-Mongoid.load!( File.dirname(__FILE__) + "/../mongoid.yml")
+Mongoid.load!( File.dirname(__FILE__) + "/../config/mongoid.yml")
